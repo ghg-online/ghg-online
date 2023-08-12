a1494bb35a83a623cbc49f82c7cd3cffb9c413e1
Nictheboy <nictheboy@outlook.com>
2023-08-13 03:03:26 +0800

    new fetures

---
+++ client/HttpClientHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class HttpClientHandlerForceToUseHttp1_1 : System.Net.Http.HttpClientHandler
+    {
+        protected override System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage> SendAsync(System.Net.Http.HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Version = new System.Version(1, 1);
+            var task = base.SendAsync(request, cancellationToken);
+            return task.ContinueWith(reply =>
+            {
+                reply.Result.Version = new System.Version(2, 0);
+                return reply.Result;
+            });
+        }
+
+        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Version = new System.Version(1, 1);
+            var reply = base.Send(request, cancellationToken);
+            reply.Version = new System.Version(2, 0);
+            return reply;
+        }
+    }
+
+    class HttpClientHandlerDisableSslCertificateValidation : System.Net.Http.HttpClientHandler
+    {
+        public HttpClientHandlerDisableSslCertificateValidation()
+        {
+            base.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+        }
+    }
+}
--- client/Program.cs
+++ client/Program.cs
@@ -3,16 +3,25 @@
 using Grpc.Net.Client;
 using server;
 using System.Net.Http;
+using System.Threading.Channels;
 
-
-// from http://duoduokou.com/csharp/64085357176954393069.html
-// disable ssl certificate validation
-var httpClientHandler = new HttpClientHandler();
-httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-var httpClient = new HttpClient(httpClientHandler);
-
-// connect to server, port = 2333, using https without ssl certificate validation
-var channel = GrpcChannel.ForAddress("https://127.0.0.1:2333", new GrpcChannelOptions() { HttpClient = httpClient });
+GrpcChannel channel;
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: [--local-server | --remote-server]");
+    return -1;
+}
+else if (args[0] == "--local-server")
+    channel = GrpcChannel.ForAddress("https://127.0.0.1:2333",
+        new GrpcChannelOptions { HttpHandler = new client.HttpClientHandlerDisableSslCertificateValidation() });
+else if (args[0] == "--remote-server")
+    channel = GrpcChannel.ForAddress("https://main-service-ghg-online-root-lqeqesemvw.cn-beijing.fcapp.run",
+        new GrpcChannelOptions { HttpHandler = new client.HttpClientHandlerForceToUseHttp1_1() });
+else
+{
+    Console.WriteLine("Usage: [--local-server | --remote-server]");
+    return -1;
+}
 var client = new Account.AccountClient(channel);
 
 var response1 = await client.RegisterAsync(new RegisterRequest { Username = "user", Password = "pass", ActivationCode = "test-activation-code" }).ResponseAsync;
@@ -20,3 +29,5 @@ Console.WriteLine($"Server reply: {response1}");
 
 var response2 = await client.LoginAsync(new LoginRequest { Username = "user", Password = "pass" }).ResponseAsync;
 Console.WriteLine($"Server reply: {response2}");
+
+return 0;
