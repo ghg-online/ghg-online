d8a1b01e543ec2122d1877cb1ffc3d356d744fa8
Nictheboy <nictheboy@outlook.com>
2023-08-17 17:51:35 +0800

    Enabled gRpc reflection

--- server/Program.cs
+++ server/Program.cs
@@ -4,8 +4,7 @@ using server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
-builder.Services.AddGrpcReflection(); // This allows clients to discover the service and call it, which is espicially usefor for auto-testing
-                                      // If you don't want it (for example, for security reasons), delete this line without worry
+builder.Services.AddGrpcReflection();
 builder.Services.AddScoped<IAccountLogger, AccountLogger>();
 builder.Services.AddScoped<IAccountManager, AccountManager>();
 builder.Services.AddScoped<IActivationCodeManager, ActivationCodeManager>();
@@ -15,5 +14,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<AccountService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
+app.MapGrpcReflectionService(); // This allows clients to discover the service and call it, which is espicially usefor for auto-testing
+                                // If you don't want it (for example, for security reasons), delete this line without worry
 app.Run();
