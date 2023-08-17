1c46bd7a33079a704bd4f6277407fdf02b03ca50
Nictheboy <nictheboy@outlook.com>
2023-08-17 17:43:52 +0800

    Add gRpc reflection

--- server/Program.cs
+++ server/Program.cs
@@ -4,12 +4,13 @@ using server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection(); // This allows clients to discover the service and call it, which is espicially usefor for auto-testing
+                                      // If you don't want it (for example, for security reasons), delete this line without worry
 builder.Services.AddScoped<IAccountLogger, AccountLogger>();
 builder.Services.AddScoped<IAccountManager, AccountManager>();
 builder.Services.AddScoped<IActivationCodeManager, ActivationCodeManager>();
 builder.Services.AddScoped<IDbHolder, DbHolder>();
 builder.Services.AddScoped<ITransactionManager, TransactionManager>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
