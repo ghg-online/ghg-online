8d001d30907e0381607488a5d4d8a5a576bccf70
Nictheboy <nictheboy@outlook.com>
2023-08-25 18:30:26 +0800

    Add new data structures and APIs for computer, directory and file. However, they are not implemented yet.

---
+++ server/Entities/Computer.cs
@@ -0,0 +1,19 @@
+﻿namespace server.Entities
+{
+    public class Computer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public Guid Owner { get; set; }
+        public Guid RootDirectory { get; set; }
+
+        public Computer(Guid owner, string name, Guid rootDirectory)
+        {
+            Owner = owner;
+            Name = name;
+            RootDirectory = rootDirectory;
+        }
+    }
+}
---
+++ server/Entities/File.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Entities
+{
+    public class File
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public Guid Parent { get; set; }
+        public TypeCode Type { get; set; }
+
+        [System.Flags]
+        public enum TypeCode
+        {
+            Readable = 1,
+            Writable = 2,
+            Executable = 4,
+            Invokable = 8,
+        }
+
+        public File(Guid id, string name, bool isDeleted, Guid parent, TypeCode type)
+        {
+            Id = id;
+            Name = name;
+            IsDeleted = isDeleted;
+            Parent = parent;
+            Type = type;
+        }
+    }
+}
