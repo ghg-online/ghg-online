8874376ee0c70619d71a7f521323280d4a3659e8
Nictheboy <nictheboy@outlook.com>
2023-08-25 17:46:05 +0800

    New APIs for computer, directory and file. However, it's not implemented.

---
+++ server/Entities/Directory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Entities
+{
+    public class Directory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public Guid Parent {  get; set; }
+
+        public Directory(Guid id, string name, bool isDeleted, Guid parent)
+        {
+            Id = id;
+            Name = name;
+            IsDeleted = isDeleted;
+            Parent = parent;
+        }
+    }
+}
