27a0d6d437d667da71bd2d115e6b5544008cfa2a
Nictheboy <nictheboy@outlook.com>
2023-10-09 00:11:09 +0800

    Improve console efficiency

--- client/Gui/Console/ScreenDriver.cs
+++ client/Gui/Console/ScreenDriver.cs
@@ -8,32 +8,30 @@ namespace client.Gui.Console
         readonly ConsoleScreen consoleScreen;
         readonly int width;
         readonly int height;
-        readonly ScreenCell[,] buffer;
+        //readonly ScreenCell[,] buffer;
 
         public ScreenDriver(ConsoleScreen consoleScreen)
         {
             this.consoleScreen = consoleScreen;
             width = consoleScreen.DataWidth;
             height = consoleScreen.DataHeight;
-            buffer = new ScreenCell[width, height];
+            //buffer = new ScreenCell[width, height];
         }
 
         public int Width => width;
 
         public int Height => height;
 
-        public void Redraw()
-        {
-            for (int y = 0; y < height; y++)
-                for (int x = 0; x < width; x++)
-                    consoleScreen[x, y] = buffer[x, y];
-        }
+        public void Redraw() { }
 
         public void Update(int x, int y, char c, SysColor foreground, SysColor background)
         {
-            buffer[x, y].Character = c;
-            buffer[x, y].Foreground = foreground;
-            buffer[x, y].Background = background;
+            consoleScreen[x, y] = new ScreenCell()
+            {
+                Character = c,
+                Foreground = foreground,
+                Background = background,
+            };
         }
 
         public void UpdateCursor(int x, int y, bool show)
